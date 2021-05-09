4e31aab909e6052311f5340816089e1ee1436340
TRAN_QUANG_DO <do.tranquang@neo-career.co.jp>
2021-05-09 20:17:14 +0900

    20210509 20:16

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# puppeteer-get-href-title
+# puppeteer-get-href-title 🐳
+
+![License](https://img.shields.io/github/license/tquangdo/puppeteer-get-href-title?color=f05340)
+
+## deploy local
 1/ npm i 2/nodemon index
+
+## URL1: https://www.easycompare.co.th/quote-form?formID=HP-CMB-TH
+![URL1](screenshot/URL1.png)
+
+## URL3: https://24h.com.vn
+### option 1)~3)
+![URL](screenshot/URL.png)
+### option 4)
+- output on console: `Tàu metro đầu tiên cập bến TP.HCM vào ngày 10/10 tới`
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// !!!!!!!! CHU Y!!!
+// 1. KO chay duoc alert()
+// 2. KO duoc tao file const.js
+
 const puppeteer = require('puppeteer'); //phải có ";"
 
 async function getData(arg_classname, arg_page) {
@@ -35,54 +39,58 @@ async function chooseYearBrandModel(arg_page) {
 
 (async () => {
     try {
-        const browser = await puppeteer.launch({
-            headless: false,
-        })
-        const page = await browser.newPage()
-        await page.goto('https://www.easycompare.co.th/')
-        await getData('.quote-year', page)
-        await getData('.quote-brand', page)
-        await chooseYearBrandModel(page)
-        await getData('.quote-model', page)
-
-        await browser.close()
-
+        // ~~~~~~~~~~ URL1 START
         // const browser = await puppeteer.launch({
         //     headless: false,
-        //     // slowMo: 200,
         // })
         // const page = await browser.newPage()
-        // // await page.goto('https://page.auctions.yahoo.co.jp/jp/auction/e463685456')
-        // // await page.waitForSelector('.ProductInformation__items > .ProductInformation__item > .Price > .Price__body > .Price__value')
-        // // const tweets = await page.$$('.ProductInformation__items > .ProductInformation__item > .Price > .Price__body > .Price__value')
-        // // for (let i = 0; i < tweets.length; i++) {
-        // //     const tweet = await (await tweets[i].getProperty('innerText')).jsonValue()
-        // //     console.log(tweet)
-        // // }
-        // await page.goto('https://24h.com.vn')
+        // await page.goto('https://www.easycompare.co.th/quote-form?formID=HP-CMB-TH')
+        // await getData('.quote-year', page)
+        // await getData('.quote-brand', page)
+        // await chooseYearBrandModel(page)
+        // await getData('.quote-model', page)
+        // await browser.close()
+        // ~~~~~~~~~~ URL1 END
 
-        // //1)
-        // // page.setViewport({ width: 1280, height: 720 })
-        // // // chụp ảnh màn hình và lưu lại với tên 24h.png
-        // // await page.screenshot({ path: '24h.png' })
+        const browser = await puppeteer.launch({
+            headless: false,
+            // slowMo: 200,
+        })
+        const page = await browser.newPage()
+        // ~~~~~~~~~~ URL2 START
+        // await page.goto('https://page.auctions.yahoo.co.jp/jp/auction/e463685456')
+        // await page.waitForSelector('.ProductInformation__items > .ProductInformation__item > .Price > .Price__body > .Price__value')
+        // const tweets = await page.$$('.ProductInformation__items > .ProductInformation__item > .Price > .Price__body > .Price__value')
+        // for (let i = 0; i < tweets.length; i++) {
+        //     const tweet = await (await tweets[i].getProperty('innerText')).jsonValue()
+        //     console.log(tweet)
+        // }
+        // ~~~~~~~~~~ URL2 END
+        // ~~~~~~~~~~ URL3 START: 1) ~ 4)(default) options
+        await page.goto('https://24h.com.vn')
 
-        // //2)
-        // // const dimensions = await page.evaluate(() => { //trong hàm evaluate() KO được gọi biến định nghĩa từ ngoài vào!!!
-        // //     return {
-        // //         width: document.documentElement.clientWidth,
-        // //         height: document.documentElement.clientHeight,
-        // //         deviceScaleFactor: window.devicePixelRatio
-        // //     }
-        // // })
-        // // console.log('Dimensions:', dimensions)
+        //1)
+        page.setViewport({ width: 1280, height: 720 })
+        // chụp ảnh màn hình và lưu lại với tên 24h.png
+        await page.screenshot({ path: '24h.png' })
 
-        // //3)
-        // // page.on('console', msg => console.log(msg.text()))
-        // // await page.evaluate(() => console.log(`url is ${location.href}`))
+        // 2)
+        const dimensions = await page.evaluate(() => { //trong hàm evaluate() KO được gọi biến định nghĩa từ ngoài vào!!!
+            return {
+                width: document.documentElement.clientWidth,
+                height: document.documentElement.clientHeight,
+                deviceScaleFactor: window.devicePixelRatio
+            }
+        })
+        console.log('Dimensions:', dimensions)
+
+        // 3)
+        page.on('console', msg => console.log(msg.text()))
+        await page.evaluate(() => console.log(`url is ${location.href}`))
 
         // //4)
         // const articles = await page.evaluate(() => {
-        //     /*
+        //     /* Chi dung de giai thich KQ
         //     * <div class="colLt" >
         //     * <header class="nwsTit cltitbxbdtt" id="id_class_title_box_bd_tt_2">
         //         <a   href="https://www.24h.com.vn/tin-tuc-trong-ngay/tau-metro-dau-tien-cap-ben-tphcm-vao-ngay-5-10-toi-c46a1182722.html" title="Tàu metro đầu tiên cập bến TP.HCM vào ngày 10/10 tới" style="font-size: 14px;">Tàu metro đầu tiên cập bến TP.HCM vào ngày 10/10 tới</a>
@@ -96,34 +104,37 @@ async function chooseYearBrandModel(arg_page) {
         //             title: item.getAttribute('title').trim(),
         //         })
         //     })
-        //     return arrTieude
+        //     return arrTieude // => xuat ra console: "Tàu metro đầu tiên cập bến TP.HCM vào ngày 10/10 tới"
         // })
         // const promises = [];
         // for (let i = 0; i < articles.length; i++) {
         //     promises.push(await getTitle(articles[i].href, page, articles[i].title))
         // }
-        // // console.log(articles)
+        // console.log(articles)
 
-        // await browser.close()
+        await browser.close()
+        // ~~~~~~~~~~ URL3 END
     } catch (err) {
         console.log("ERR!!! " + err)
     }
 })()
 
-// async function getTitle(arg_link, arg_page, arg_title) {
-//     await arg_page.goto(arg_link, {
-//         timeout: 3000000
-//     })
-//     // Chờ 1s sau khi page được load để tránh overload
-//     await arg_page.waitForTimeout(1000)
+// ~~~~~~~~~~ URL3 START
+async function getTitle(arg_link, arg_page, arg_title) {
+    await arg_page.goto(arg_link, {
+        timeout: 3000000
+    })
+    // Chờ 1s sau khi page được load để tránh overload
+    await arg_page.waitForTimeout(1000)
 
-//     let title = await arg_page.evaluate(() => {
-//         let artTitle = document.querySelector("div.clF h2.ctTp")
-//         let kq = (artTitle) ? artTitle.innerText : '6969'
-//         return kq
-//     })
+    let title = await arg_page.evaluate(() => {
+        let artTitle = document.querySelector("div.clF h2.ctTp")
+        let kq = (artTitle) ? artTitle.innerText : '6969'
+        return kq
+    })
 
-//     title = (title === '6969') ? arg_title : title
-//     console.log(title)
-//     return arg_page
-// }
+    title = (title === '6969') ? arg_title : title
+    console.log(title)
+    return arg_page
+}
+// ~~~~~~~~~~ URL3 END
